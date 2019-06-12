5b1c2b06178bd8150ad6358f1bf28d60ea7329a8
yashrajsamaiyamind <46667907+yashrajsamaiyamind@users.noreply.github.com>
2019-06-12 11:42:52 +0530

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -1,4 +1,8 @@
-exports.handler = (event, context, callback) => {
-    // Succeed with the string "Hello world!"
-    callback(null, 'Hello world!');
-};
+    num1 = 15
+    num2 = 12
+  
+    # Adding two nos 
+    sum = num1 + num2 
+  
+    # printing values 
+    print("Sum of {0} and {1} is {2}" .format(num1, num2, sum))
