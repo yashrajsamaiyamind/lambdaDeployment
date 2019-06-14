0a29a9d7a7612aba9f7fe3af28fa40045de5154c
yashrajsamaiyamind <46667907+yashrajsamaiyamind@users.noreply.github.com>
2019-06-14 16:49:29 +0530

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -1,8 +1,18 @@
-    num1 = 15
-    num2 = 12
+import json
+
+
+ 
   
-    # Adding two nos 
-    sum = num1 + num2 
-  
-    # printing values 
-    print("Sum of {0} and {1} is {2}" .format(num1, num2, sum))
+
+def lambda_handler(event, context):
+    def sumOfSeries(n): 
+        sum = 0
+        for i in range(1, n+1):
+            sum +=i*i*i 
+        return sum
+    n = 5
+    print(sumOfSeries(n))
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
